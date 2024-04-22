e0ed5ee3e3d11d0ef1eb16a9ff59248545f6be28
babu <babunabik@gmail.com>
2024-04-22 19:42:52 +0530

    handled button press state

--- lib/widgets/app_neumorphic_button.dart
+++ lib/widgets/app_neumorphic_button.dart
@@ -3,38 +3,65 @@ import 'package:yuktidea/utils/config/size_config.dart';
 
 import '../resources/color.dart';
 
-class AppNeumorphicButton extends StatelessWidget {
+class AppNeumorphicButton extends StatefulWidget {
   final Function()? onPressed;
   final String text;
 
   const AppNeumorphicButton({
-    super.key,
+    Key? key,
     this.onPressed,
     required this.text,
-  });
+  }) : super(key: key);
+
+  @override
+  _AppNeumorphicButtonState createState() => _AppNeumorphicButtonState();
+}
+
+class _AppNeumorphicButtonState extends State<AppNeumorphicButton> {
+  bool _isPressed = false;
 
   @override
   Widget build(BuildContext context) {
     return IntrinsicWidth(
       child: SizedBox(
         height: 60.h,
-        child: NeumorphicButton(
-          onPressed: onPressed,
-          style: NeumorphicStyle(
-            shape: NeumorphicShape.flat,
-            boxShape: NeumorphicBoxShape.roundRect(
-              BorderRadius.circular(50),
+        child: GestureDetector(
+          onTapDown: (_) {
+            setState(() {
+              _isPressed = true;
+              widget.onPressed?.call();
+            });
+          },
+          onTapUp: (_) {
+            setState(() {
+              _isPressed = false;
+            });
+          },
+          onTapCancel: () {
+            setState(() {
+              _isPressed = false;
+            });
+          },
+          child: Neumorphic(
+            style: NeumorphicStyle(
+              shape: NeumorphicShape.flat,
+              boxShape: NeumorphicBoxShape.roundRect(
+                BorderRadius.circular(50),
+              ),
+              intensity: _isPressed ? 1 : 0.5,
             ),
-          ),
-          child: Container(
-            margin: EdgeInsets.symmetric(horizontal: 30),
-            alignment: Alignment.center,
-            child: Text(
-              text,
-              style: TextStyle(
-                fontSize: 20.sp,
-                fontWeight: FontWeight.w400,
-                color: AppColor.primary.withGreen(400),
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 30),
+              alignment: Alignment.center,
+              child: Text(
+                widget.text,
+                style: TextStyle(
+                  fontSize: 20.sp,
+                  fontWeight: FontWeight.w400,
+                  color: _isPressed
+                      ? AppColor.primary.withOpacity(0.7)
+                      : AppColor.primary.withGreen(400),
+                ),
               ),
             ),
           ),
